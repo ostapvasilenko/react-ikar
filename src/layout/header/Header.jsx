ddccb867282af1772491ff87258abd4127641223
ostapvasulenko <ostapvasilenko55@gmail.com>
2023-02-25 13:32:35 +0200

    1

--- src/layout/header/Header.jsx
+++ src/layout/header/Header.jsx
@@ -1,24 +1,22 @@
 import React from 'react'
 import styles from './Header.module.css'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <header className={styles.header}>Header
-    
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-      </ul>
-      <ul>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
-    </nav>
-    
+
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+
     </header>
   )
 }
