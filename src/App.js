ce60af9c136f394efc69bda65715dd43f13a1b01
ostapvasulenko <ostapvasilenko55@gmail.com>
2023-02-25 13:19:16 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { BrowserRouter} from "react-router-dom";
 import RouterApp from "./router";
-import NavBar from "./components/UI/navBar/navBar";
 
 function App() {
   return (
     <BrowserRouter>
-      <NavBar />
       <RouterApp />
     </BrowserRouter>
   );
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,17 +1,15 @@
-/*import React from "react";
-import About from "../pages/About";
-import Posts from "../pages/Posts";
-import SinglePostPage from "../pages/SinglePostPage";
+import React from "react";
+import Home from "../pages/Home";
 import { Route, Routes } from "react-router-dom";
 
 const RouterApp = () => {
   return (
     <Routes>
-      <Route exact path="/posts/:id" element={<SinglePostPage />}></Route>
-      <Route path="/about" element={<About />}></Route>
-      <Route exact path="/posts" element={<Posts />}></Route>
+      {/* { <Route exact path="/posts/:id" element={<SinglePostPage />}></Route>
+      <Route path="/about" element={<About />}></Route> } */}
+      <Route exact path="/" element={<Home />}></Route>
     </Routes>
   );
 };
 
-export default RouterApp;*/
+export default RouterApp;
