f8c60495150126f85a35ec7eabe89a36f8455c8d
ostapvasulenko <ostapvasilenko55@gmail.com>
2023-02-26 14:20:32 +0200

    1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,34 +1,65 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
 const Home = () => {
   let [news, setNews] = useState([]);
+  let [primaryPost, setPrimaryPost] = useState([]);
 
   useEffect(() => {
     getNews();
+    getPrimaryPost();
   }, []);
 
   useEffect(() => {
-    console.log(news)
-  }, [news]);
+    console.log(news);
+    console.log(primaryPost);
+  }, [news, primaryPost]);
 
+  const BASE_URL = "https://admin.ikar-thinktank.org/api";
+  const getNews = async () => {
+    await fetch(`${BASE_URL}/news`)
+      .then((response) => response.json())
+      .then((res) => {
+        const CLEARED_DATA = res.data.map((item) => {
+          return {
+            id: item.id,
+            ...item.attributes,
+          };
+        });
+        setNews(CLEARED_DATA);
+      });
+  };
 
-  const BASE_URL = 'https://admin.ikar-thinktank.org/api';
-  const getNews = () => {
-    fetch(`${BASE_URL}/news`)
-      .then(response => response.json())
-      .then(data => {
-        data.data.map((item) => {
-        return{
-          id: item.id,...item.atributes
-        }
-        })
-        console.log()
-        // setNews(data.data);
+  const getPrimaryPost = async () => {
+    await fetch(`${BASE_URL}/primary-post`)
+      .then((response) => response.json())
+      .then((res) => {
+        console.log(res);
+        // const CLEARED_DATA = res.data.map((item) => {
+        //   return {
+        //     id: item.id,
+        //     ...item.attributes,
+        //   };
+        // });
+        // setPrimaryPost(CLEARED_DATA);
+      })
+      .catch((err) => {
+        console.log(err);
       })
   };
-  return (
-    <div>Home</div>
-  )
-}
 
-export default Home
+  return (
+    <>
+      <div>Home</div>
+      {news.map((item) => {
+        return (
+          <div key={item.id}>
+            <p>{item.title}</p>
+            <p>{item.text}</p>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default Home;
